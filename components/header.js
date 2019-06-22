f9582b6f5c65b2e513ca8bca0c3c4a584e5d12b6
Rocco Zanni <rocco.zanni@spreaker.com>
2019-06-22 20:44:05 +0200

    Moved to next.js and now

---
+++ components/header.js
@@ -0,0 +1,5 @@
+export default () => (
+  <header>
+    <h1>Hello there!</h1>
+  </header>
+);
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default () => (
+  <main>
+    <Header />
+    <section>
+      I'm Rocco, Technology addicted and CTO at <a href="https://www.voxnest.com">Voxnest</a>.
+    </section>
+    <Footer />
+  </main>
+);
